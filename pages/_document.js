0a8ec8b891a86d1d2ff2642c1946402171bed4fa
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-12-19 11:03:06 +0000

    Init

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { globalStyles } from '../components/styles';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charSet='utf-8' />
+          <link
+            href='https://fonts.googleapis.com/css?family=Quicksand:300,400,700'
+            rel='stylesheet'
+          />
+          {this.props.styleTags}
+          <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
