7a64eeb8a2601abb2862acd51b2b8b2d15a7b5bd
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-01-03 00:58:37 +0000

    :bento: Add Google Font

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props =>
-      sheet.collectStyles(<App {...props} />)
+      sheet.collectStyles(<App {...props} />),
     );
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
@@ -17,10 +17,10 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta charSet='utf-8' />
+          <meta charSet="utf-8" />
           <link
-            href='https://fonts.googleapis.com/css?family=Quicksand:300,400,700'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700"
+            rel="stylesheet"
           />
           {this.props.styleTags}
           <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
