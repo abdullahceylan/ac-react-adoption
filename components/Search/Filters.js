ed5b112360ecf621146db59a6ed1c741f228e168
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-01-05 00:44:25 +0000

    :art: Add search filters

---
+++ components/Search/Filters.js
@@ -0,0 +1,42 @@
+const iconBase = '/static/images/icons';
+
+const filters = {
+  animals: [
+    {
+      id: 'Cat',
+      name: 'Cat',
+      iconURL: `${iconBase}/005-cat-1.svg`,
+      iconURLSelected: `${iconBase}/005-cat-2.svg`,
+    },
+    {
+      id: 'Dog',
+      name: 'Dog',
+      iconURL: `${iconBase}/001-dog-1.svg`,
+      iconURLSelected: `${iconBase}/001-dog-2.svg`,
+    },
+    {
+      id: 'Horse',
+      name: 'Horse',
+      iconURL: `${iconBase}/001-horse-1.svg`,
+      iconURLSelected: `${iconBase}/001-horse-2.svg`,
+    },
+    {
+      id: 'Bird',
+      name: 'Bird',
+      iconURL: `${iconBase}/009-bird-1.svg`,
+      iconURLSelected: `${iconBase}/009-bird-2.svg`,
+    },
+    {
+      id: 'Reptile',
+      name: 'Reptile',
+      iconURL: `${iconBase}/011-snake-1.svg`,
+      iconURLSelected: `${iconBase}/011-snake-2.svg`,
+    },
+    {
+      id: 'Smallfurry',
+      name: 'Smallfurry',
+      iconURL: `${iconBase}/013-rabbit-1.svg`,
+      iconURLSelected: `${iconBase}/013-rabbit-2.svg`,
+    },
+  ],
+};
