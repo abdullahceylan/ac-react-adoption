27be8470e6b8745995db0c49f37fadc6c76852ba
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-01-13 22:21:46 +0000

    :art: Complete ShelterPets component

--- components/ShelterPets/ShelterPets.jsx
+++ components/ShelterPets/ShelterPets.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import { map } from 'lodash/fp';
 import { Query } from 'react-apollo';
 import PetCard from '@components/PetCard';
+import Placeholder from '@components/Placeholder';
 import { shelterGetPetsQuery } from '@queries';
+import { loadMoreContent } from '@helpers';
 import { ShelterPetsWrapper, ShelterPetsList } from './ShelterPets.styles';
-import { Col } from '@styles';
+import { Col, Button } from '@styles';
 // import ShelterPetsList from './data.json';
 
 const ShelterPets = ({ shelterId }) => {
@@ -16,14 +18,33 @@ const ShelterPets = ({ shelterId }) => {
   const shelterGetPetsQueryVariables = {
     shelterId: shelterId,
     status: 'A',
-    count: 10,
+    count: 8,
+    offset: 0,
   };
 
   return (
-    <Query query={shelterGetPetsQuery} variables={shelterGetPetsQueryVariables}>
+    <Query
+      query={shelterGetPetsQuery}
+      variables={shelterGetPetsQueryVariables}
+      notifyOnNetworkStatusChange
+    >
       {({ loading, error, data, fetchMore }) => {
         if (error) return <div>Error loading pets.</div>;
-        if (loading) return <div>Loading</div>;
+        if (loading) {
+          return (
+            <ShelterPetsWrapper>
+              <ShelterPetsList>
+                {Array(shelterGetPetsQueryVariables.count)
+                  .fill('')
+                  .map((p, i) => (
+                    <Col key={i} xs={12} sm={3} md={3}>
+                      <Placeholder />
+                    </Col>
+                  ))}
+              </ShelterPetsList>
+            </ShelterPetsWrapper>
+          );
+        }
 
         if (!data.shelterGetPets) {
           return null;
@@ -41,29 +62,23 @@ const ShelterPets = ({ shelterId }) => {
                 data.shelterGetPets.pets,
               )}
             </ShelterPetsList>
+            <Button
+              onClick={() =>
+                loadMoreContent({
+                  queryType: 'shelterGetPets',
+                  data: data.shelterGetPets.pets,
+                  fetchMore,
+                })
+              }
+            >
+              Load more
+            </Button>
           </ShelterPetsWrapper>
         );
       }}
     </Query>
   );
 };
-
-function loadMorePosts(petFind, fetchMore) {
-  fetchMore({
-    variables: {
-      skip: petFind.length,
-    },
-    updateQuery: (previousResult, { fetchMoreResult }) => {
-      if (!fetchMoreResult) {
-        return previousResult;
-      }
-      return Object.assign({}, previousResult, {
-        // Append the new posts results to the old one
-        shelterGetPets: [...previousResult.petFind, ...fetchMoreResult.petFind],
-      });
-    },
-  });
-}
 
 ShelterPets.propTypes = {
   shelterId: PropTypes.string.isRequired,
--- queries/shelterGetPets.query.js
+++ queries/shelterGetPets.query.js
@@ -1,9 +1,20 @@
 import gql from 'graphql-tag';
 
 const shelterPetsQuery = gql`
-  query shelterGetPets($shelterId: String!, $count: Int, $status: String) {
-    shelterGetPets(shelterId: $shelterId, count: $count, status: $status) {
+  query shelterGetPets(
+    $shelterId: String!
+    $count: Int
+    $offset: Int
+    $status: String
+  ) {
+    shelterGetPets(
+      shelterId: $shelterId
+      count: $count
+      offset: $offset
+      status: $status
+    ) {
       lastOffset
+      isLastRecord
       pets {
         id
         name
